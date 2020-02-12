d2b2553d9316134a6388c6a5e4bde2a1601f8927
Ben Faerber <benfaerber@protonmail.com>
2020-02-12 13:03:55 -0700

    Created panel

--- src/button/view/SampleFrame.java
+++ src/button/view/SampleFrame.java
@@ -1,12 +1,26 @@
 package button.view;
 
 import button.controller.Controller;
-
-public class SampleFrame
+import javax.swing.JFrame;
+public class SampleFrame extends JFrame
 {
 	private Controller controller;
+	private SamplePanel panel;
+	
 	public SampleFrame(Controller controller)
 	{
+		super();
 		this.controller = controller;
+		this.panel = new SamplePanel(this.controller);
+		setupFrame();
+	}
+	
+	private void setupFrame()
+	{
+		this.setContentPane(panel);
+		this.setTitle("Sample GUI App");
+		this.setResizable(true);
+		this.setSize(800,600);
+		this.setVisible(true);
 	}
 }
---
+++ src/button/view/SamplePanel.java
@@ -0,0 +1,13 @@
+package button.view;
+
+import button.controller.Controller;
+import javax.swing.JPanel;
+public class SamplePanel extends JPanel
+{
+	private Controller controller;
+	public SamplePanel(Controller controller)
+	{
+		super();
+		this.controller = controller;
+	}
+}
